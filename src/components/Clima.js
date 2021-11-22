ecf678d6e5f5c9f0acb6ec54698293cea593589d
Your Name <you@example.com>
2021-11-21 18:19:34 -0600

    Clima en React consultado a openweathermap.org CURSO

---
+++ src/components/Clima.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Clima = ({response}) => {
+
+    //extraer valores
+    const {name, main} = response;
+
+    if(!name) return null;
+
+    //grados kelvin
+    const kelvin = 273.15;
+
+    return (
+        <div className="card-panel white col s12">
+            <div className="black-text">
+                <h2>El clima de {name} es: </h2>
+                <p className="temperatura">
+                    {parseFloat(main.temp - kelvin, 10).toFixed(2)} <span> &#x2103; </span>
+                </p>
+                <p>Temperatura máxima: 
+                    {parseFloat(main.temp_max - kelvin, 10).toFixed(2)} <span> &#x2103; </span>
+                </p>
+                <p>Temperatura mínima: 
+                    {parseFloat(main.temp_min - kelvin, 10).toFixed(2)} <span> &#x2103; </span>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+Clima.propTypes = {
+    response: PropTypes.object.isRequired
+}
+
+export default Clima
